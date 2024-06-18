892dc53b434217ed1afd9274977a70796ad07021
David Bolt <David.Bolt@Sfcknox.org>
2024-06-18 17:13:42 -0400

    change log format from csv to .nsj so database can view directly

--- logs.js
+++ logs.js
@@ -37,11 +37,16 @@ async init() {
     // creatre a new directory for the day
     const dir = app.config.logDir +"/"+ new Date().toISOString().slice(0,10);
     await app.verifyPath(dir);
-    //this.fileStatus = 0;
 
     this.fsError    = app.fs.createWriteStream(dir + "/error.njs"   , {flags: 'a'});
+    this.write( this.fsError,"h",  ["session #", "request#", "messsage"] );
+
     this.fsRequest  = app.fs.createWriteStream(dir + "/request.njs" , {flags: 'a'});
+    this.write(this.fsRequest,"h",["session #", "request #", "method", "host", "url"]);
+
     this.fsResponse = app.fs.createWriteStream(dir + "/response.njs", {flags: 'a'});
+    this.write(this.fsResponse,"h", ["Session #", "Request #", "Start", "Last Request", "Duration", "ip", "Method", "URL", "bytes"]);
+
     this.summaryFile=                          dir + "/summary.json";  // string for location of log file
 
     await app.sessions.initSummary(this.summaryFile);
@@ -50,12 +55,6 @@ async init() {
     console.log("logClass.init err="+e);
   }
 }
-
-/*
-ready(file) {  //  logClass - server-side
-  console.log(++this.fileStatus);
-}
-*/
 
 
 error(msg, request, response) {  //  logClass - server-side
@@ -66,18 +65,18 @@ error(msg, request, response) {  //  logClass - server-side
     sessionNumber = response.synergyRequest.sessionNumber;
     requestNumber = response.synergyRequest.requestNumber;
   }
-  this.write( this.fsError,  [sessionNumber, requestNumber, msg] );
+  this.write( this.fsError,"a",  [sessionNumber, requestNumber, msg] );
 }
 
 
 request(request,response) {  //  logClass - server-side
   // append message to log file
-  this.write(this.fsRequest,[this.to_number(response.synergyRequest.sessionNumber), response.synergyRequest.requestNumber, request.method, request.headers.host, request.url]);
+  this.write(this.fsRequest,"a",[this.to_number(response.synergyRequest.sessionNumber), response.synergyRequest.requestNumber, request.method, request.headers.host, request.url]);
 }
 
 
 response(obj){  //  logClass - server-side
-  this.write(this.fsResponse, [this.to_number(obj.sessionKey), obj.requestNum, obj.start, obj.lastRequest, obj.duration, obj.ip, obj.method, obj.url, obj.bytesSent]);
+  this.write(this.fsResponse,"a", [this.to_number(obj.sessionKey), obj.requestNum, obj.start, obj.lastRequest, obj.duration, obj.ip, obj.method, obj.url, obj.bytesSent]);
 }
 
 
@@ -119,9 +118,22 @@ async summary() {
 }
 
 
-write(stream, json_array){
+write(stream, command, json_array){
   // adding date and convert to CSV format (commas and quotes to mssage)
-  json_array.push(new Date().toISOString());  // add server timespame
+  switch (command) {
+    case "a":  // append
+      json_array.unshift(new Date().toISOString(),command);  // add server timestamp and "append" command
+      break;
+
+    case "h": // header
+      json_array.unshift(new Date().toISOString(),command,"Time Stamp", "Command");  // add server timestamp and "append" command
+    break; 
+
+    default:   // error
+      // add error code
+      break;
+  }
+
   const m = JSON.stringify(json_array) + `\n`  // add newline so file can be parsed
 
   if (stream) {
